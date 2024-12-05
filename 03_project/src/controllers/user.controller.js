17b541e4e560239bfefcc4520c52d07a16eff5ce
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-12-05 22:02:54 +0545

    login and logout configurations

--- 03_project/src/controllers/user.controller.js
+++ 03_project/src/controllers/user.controller.js
@@ -4,6 +4,16 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 
+const generateAccessAndRefreshToken = async (userId) => {
+  const user = await User.findById(userId);
+  const accessToken = user.generateAccessToken();
+  const refreshToken = user.generateRefreshToken();
+  user.refreshToken = refreshToken;
+
+  await user.save({ validateBeforeSave: false });
+  return { accessToken, refreshToken };
+};
+
 const registerUser = asyncHandler(async (req, res) => {
   const { username, email, fullName, password } = req.body;
 
@@ -39,7 +49,6 @@ const registerUser = asyncHandler(async (req, res) => {
     avatar: avatarUrl.url,
     coverImage: coverImageUrl ? coverImageUrl.url : "",
   });
-  console.log(userResponse);
   // '-' sign used to remove the fields form the selected fields
   const createdUser = await User.findById(userResponse._id).select(
     "-password -refreshToken"
@@ -53,4 +62,72 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, username, password } = req.body;
+
+  if (!email || !username) {
+    throw new ApiError(400, "username or email is required");
+  }
+
+  const user = await User.findOne({ $or: [{ username }, { email }] });
+  if (!user) {
+    throw new ApiError(404, "User doesnot exist");
+  }
+
+  const isPasswordValid = user.isPasswordCorrect(password);
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
+
+  const loggedInUser = await User.findOne(user._id).select(
+    "-Password -refreshToken"
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, opitons)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser.accessToken,
+          refreshToken,
+        },
+        "user logged in successfully"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .clearCookie("accessToken", accessToken, options)
+    .clearCookie("refreshToken", refreshToken, options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
+});
+
+export { registerUser, loginUser, logoutUser };
--- 03_project/src/utils/cloudinary.js
+++ 03_project/src/utils/cloudinary.js
@@ -14,12 +14,40 @@ const uploadOnCloudinary = async (localFilePath) => {
     const res = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-    fs.unlinkSync(localFilePath);
+
+    if (fs.existsSync(localFilePath)) {
+      fs.unlinkSync(localFilePath);
+    }
+
     return res;
   } catch (error) {
-    fs.unlinkSync(localFilePath);
+    console.error("Cloudinary upload failed:", error.message);
+
+    if (fs.existsSync(localFilePath)) {
+      try {
+        fs.unlinkSync(localFilePath);
+      } catch (err) {
+        console.error("Error deleting file after failure:", err.message);
+      }
+    }
+
     return null;
   }
 };
+
+// const uploadOnCloudinary = async (localFilePath) => {
+//   try {
+//     if (!localFilePath) return null;
+
+//     const res = await cloudinary.uploader.upload(localFilePath, {
+//       resource_type: "auto",
+//     });
+//     fs.unlinkSync(localFilePath);
+//     return res;
+//   } catch (error) {
+//     fs.unlinkSync(localFilePath);
+//     return null;
+//   }
+// };
 
 export { uploadOnCloudinary };
